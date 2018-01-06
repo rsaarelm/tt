e8fa4bfe009ab85af24e01d7dbc040bce8fda343
Risto Saarelma <risto.saarelma@iki.fi>
2018-01-06 08:56:07 +0200

    Hlint fixes

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -5,19 +5,19 @@ import Test.Hspec
 import qualified Data.Time as Time
 
 main :: IO ()
-main = hspec $ do
+main = hspec $
   describe "Tt.parseToken" $ do
-    it "parses regular words" $ do
+    it "parses regular words" $
       parseToken "foo" `shouldBe` Word "foo"
-    it "parses date" $ do
+    it "parses date" $
       parseToken "2012-12-21" `shouldBe` (Date $ Time.fromGregorian 2012 12 21)
-    it "parses time of day without seconds" $ do
+    it "parses time of day without seconds" $
       parseToken "18:32" `shouldBe` (Time $ Time.TimeOfDay 18 32 00)
-    it "parses time of day with seconds" $ do
+    it "parses time of day with seconds" $
       parseToken "18:32:43" `shouldBe` (Time $ Time.TimeOfDay 18 32 43)
-    it "parses project identifiers" $ do
-      parseToken "+stuff" `shouldBe` (Project "stuff")
-    it "parses attributes with string values" $ do
-      parseToken "prio:C" `shouldBe` (Attr "prio" $ Word "C")
-    it "parses attributes with date values" $ do
-      parseToken "due:2018-03-20" `shouldBe` (Attr "due" $ Date $ Time.fromGregorian 2018 3 20)
+    it "parses project identifiers" $
+      parseToken "+stuff" `shouldBe` Project "stuff"
+    it "parses attributes with string values" $
+      parseToken "prio:C" `shouldBe` Attr "prio" (Word "C")
+    it "parses attributes with date values" $
+      parseToken "due:2018-03-20" `shouldBe` Attr "due" (Date $ Time.fromGregorian 2018 3 20)
