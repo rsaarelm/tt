27401413f92dcde57cda1d540d792e2acb4aca8d
Risto Saarelma <risto.saarelma@iki.fi>
2018-01-06 17:58:58 +0200

    Hide helper methods from public namespace

--- src/Tt.hs
+++ src/Tt.hs
@@ -2,7 +2,6 @@ module Tt where
 
 import qualified Data.Time as Time
 import Data.Char
-import Data.List
 import Data.Maybe
 import Control.Applicative
 
@@ -16,42 +15,41 @@ data Token =
   | Colon Token Token       -- ^ Two tokens split by colon (the whole doesn't parse into Time)
     deriving (Eq, Show)
 
--- Force time parser to return a Maybe-wrapped value of whatever time type we
--- want using the given format string.
-timeParse :: (Time.ParseTime t) => String -> String -> Maybe t
-timeParse = Time.parseTimeM True Time.defaultTimeLocale
+-- | Parse a single word (whitespace-delimited string) into a Tt Token.
+parseToken :: String -> Token
+parseToken word = fromMaybe (Text word) $ parseDate word
+                                      <|> parseTime word
+                                      <|> parseTimeSec word
+                                      <|> parseProject word
+                                      <|> parseId word
+                                      <|> parseColon word
+  where
+    parseDate :: String -> Maybe Token
+    parseDate s = Date <$> timeParse "%Y-%m-%d" s
 
-parseDate :: String -> Maybe Token
-parseDate s = Date <$> timeParse "%Y-%m-%d" s
+    parseTime :: String -> Maybe Token
+    parseTime s = Time <$> timeParse "%H:%M" s
 
-parseTime :: String -> Maybe Token
-parseTime s = Time <$> timeParse "%H:%M" s
+    parseTimeSec :: String -> Maybe Token
+    parseTimeSec s = Time <$> timeParse "%H:%M:%S" s
 
-parseTimeSec :: String -> Maybe Token
-parseTimeSec s = Time <$> timeParse "%H:%M:%S" s
+    parseProject :: String -> Maybe Token
+    parseProject ('+' : xs) = Just (Project xs)
+    parseProject _ = Nothing
 
-parseProject :: String -> Maybe Token
-parseProject ('+' : xs) = Just (Project xs)
-parseProject _ = Nothing
+    parseId :: String -> Maybe Token
+    parseId (x:xs) | isFirst x && all isSecond xs = Just (Identifier (x:xs))
+        where
+        isFirst c = c == '_' || isAlpha c
+        isSecond c = c == '_' || isAlphaNum c
+    parseId _ = Nothing
 
-parseId :: String -> Maybe Token
-parseId (x:xs) | isFirst x && all isSecond xs = Just (Identifier (x:xs))
-    where
-    isFirst c = c == '_' || isAlpha c
-    isSecond c = c == '_' || isAlphaNum c
-parseId _ = Nothing
-
-parseColon :: String -> Maybe Token
-parseColon s = case elemIndex ':' s of
-    Just idx -> case splitAt idx s of
+    parseColon :: String -> Maybe Token
+    parseColon s = case break (== ':') s of
         (x:xs, ':':y:ys) -> Just (Colon (parseToken (x:xs)) (parseToken (y:ys)))
         _ -> Nothing
-    Nothing -> Nothing
 
-parseToken :: String -> Token
-parseToken s = fromMaybe (Text s) $ parseDate s
-                                <|> parseTime s
-                                <|> parseTimeSec s
-                                <|> parseProject s
-                                <|> parseId s
-                                <|> parseColon s
+    -- Force time parser to return a Maybe-wrapped value of whatever time type we
+    -- want using the given format string.
+    timeParse :: (Time.ParseTime t) => String -> String -> Maybe t
+    timeParse = Time.parseTimeM True Time.defaultTimeLocale
