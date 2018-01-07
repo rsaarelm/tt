3a48693fc422a6afa4131be601dae821b0920898
Risto Saarelma <risto.saarelma@iki.fi>
2018-01-07 16:48:18 +0200

    Reading tokens from existing files

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,25 @@
 module Main where
 
 import Control.Monad (join)
-import System.Environment
+import System.Directory (doesFileExist, getHomeDirectory)
 import Options.Applicative
 import Data.Semigroup ((<>))
 import Tt
+
+main :: IO ()
+main = join $ execParser $ info (opts <**> helper) $
+    fullDesc
+    <> progDesc "Time tracking tool"
+    <> header "tt - time tracking tool"
+
+opts :: Parser (IO ())
+opts = subparser $
+     (command "in" $ info (in_ <$> (argument str (metavar "project")) <*> (many (argument str (metavar "description")))) $
+      progDesc "Clock in to a project")
+  <> (command "out" $ info (out <$> many (argument str (metavar "description"))) $
+      progDesc "Clock out of the clocked in project")
+  <> (command "timeclock" $ info (pure timeclock) $
+      progDesc "Output hours in timeclock format for hledger")
 
 in_ :: String -> [String] -> IO ()
 in_ project text = do
@@ -20,17 +35,19 @@ timeclock :: IO ()
 timeclock = do
     putStrLn "TODO"
 
-opts :: Parser (IO ())
-opts = subparser $
-     (command "in" $ info (in_ <$> (argument str (metavar "project")) <*> (many (argument str (metavar "description")))) $
-      progDesc "Clock in to a project")
-  <> (command "out" $ info (out <$> many (argument str (metavar "description"))) $
-      progDesc "Clock out of the clocked in project")
-  <> (command "timeclock" $ info (pure timeclock) $
-      progDesc "Output hours in timeclock format for hledger")
 
-main :: IO ()
-main = join $ execParser $ info (opts <**> helper) $
-    fullDesc
-    <> progDesc "Time tracking tool"
-    <> header "tt - time tracking tool"
+parseFile :: FilePath -> IO [[Token]]
+parseFile path = do
+    exists <- doesFileExist path
+    if exists then do
+        contents <- readFile path
+        return $ map tokenize $ lines contents
+    else do
+        return []
+
+readDatabase :: IO [[Token]]
+readDatabase = do
+    home <- getHomeDirectory
+    done <- parseFile (home ++ "/done.txt")
+    todo <- parseFile (home ++ "/todo.txt")
+    return (done ++ todo)
