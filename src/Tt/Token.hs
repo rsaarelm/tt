505f4fce63fad83e7d4df57b2837a098b9eff270
Risto Saarelma <risto.saarelma@iki.fi>
2018-01-27 13:43:47 +0200

    New Parsec-based token parser

---
+++ src/Tt/Token.hs
@@ -0,0 +1,141 @@
+module Tt.Token (
+    Token(Date, Time, Colon, Number, Priority, Sym, Comment, Text),
+    showToken,
+    tokenParser,
+) where
+
+import Control.Monad
+import Data.Char
+import Data.Fixed (Pico)
+import Data.Time
+import Numeric
+import Text.Parsec
+import Text.Parsec.String (Parser)
+import Text.Printf
+
+-- | Parts of a todo.txt line item
+data Token =
+    Date Day                    -- ^ A calendar date, YYYY-mm-dd
+
+  | Time (TimeOfDay, TimeZone)  -- ^ A time of day, HH:MM:SS-HHMM
+                                --
+                                --   (we really want ZonedTime here but it
+                                --   doesn't implement Eq which we want to
+                                --   derive for the Token type)
+
+  | Colon Token Token           -- ^ Name value pair
+  | Number Rational             -- ^ A numerical value, 1, -8 or 3.1415
+  | Priority Char               -- ^ Priority tag, "(A)" etc.
+  | Sym String                  -- ^ A symbolic identifier
+  | Comment String              -- ^ Comment text after a double hyphen
+  | Text String                 -- ^ Whitespace separated anything else
+    deriving (Eq, Show)
+
+-- | Pretty-print a Token
+showToken :: Token -> String
+showToken (Date d) = show d
+-- Don't show fractional seconds.
+showToken (Time (t, z)) = formatTime defaultTimeLocale "%H:%M:%S%z"
+                          (toZonedTime t z)
+showToken (Colon t u) = showToken t ++ ":" ++ showToken u
+showToken (Number n) = showFFloat Nothing (fromRat n :: Double) ""
+showToken (Priority ch) = printf "(%c)" ch
+showToken (Comment c) = printf "--%s" c
+showToken (Sym t) = t
+showToken (Text t) = t
+
+toZonedTime :: TimeOfDay -> TimeZone -> ZonedTime
+toZonedTime t = ZonedTime (LocalTime (ModifiedJulianDay 0) t)
+
+
+-- | Parse a string into a Token
+tokenParser :: Parser Token
+tokenParser = wrap date <|> wrap zonedTime <|> wrap colon <|> wrap number <|>
+              wrap priority <|> wrap sym <|> wrap comment <|> wrap text
+  where
+    wrap :: Parser Token -> Parser Token
+    wrap parser = try (parser <* (eof <|> void (many1 space)))
+
+date :: Parser Token
+date = Date <$> (fromGregorian <$> year <* char '-' <*> month <* char '-' <*> day)
+
+zonedTime :: Parser Token
+zonedTime = Time <$> ((,) <$> timeOfDay <*> zoneOffset)
+
+colon :: Parser Token
+colon = Colon <$> sym <* char ':' <*> (sym <|> date <|> zonedTime <|> number)
+
+number :: Parser Token
+number = Number <$> plusMinus
+
+priority :: Parser Token
+priority = Priority <$> (char '(' *> (letter <|> digit) <* char ')')
+
+sym :: Parser Token
+sym = Sym <$> symbol
+
+comment :: Parser Token
+comment = Comment <$> (string "--" *> manyTill anyChar (void (char '\n') <|> eof))
+
+text :: Parser Token
+text = Text <$> many1 (satisfy (not . isSpace))
+
+-- XXX: Currently numbers that start with + can parse as digits, this is
+-- hacked around in tokenParser by trying to parse the input as a number
+-- before trying to parse it as a symbol.
+
+symbol :: Parser String
+symbol = (:) <$> firstChar <*> many restChars
+  where
+    firstChar = letter <|> oneOf "_+@"
+    restChars = letter <|> digit <|> oneOf "_+@-"
+
+timeOfDay :: Parser TimeOfDay
+timeOfDay = TimeOfDay <$> hour <* char ':' <*> minute <* char ':' <*> second
+
+zoneOffset :: Parser TimeZone
+zoneOffset = negativeZone <|> positiveZone
+  where
+    negativeZone = (minutesToTimeZone . negate) <$> (char '-' *> hhmmToMinutes)
+    positiveZone = minutesToTimeZone <$> (char '+' *> hhmmToMinutes)
+    hhmmToMinutes = (\h m -> h * 60 + m) <$> hour <*> minute
+
+year :: Parser Integer
+year = read <$> count 4 digit
+
+month :: Parser Int
+month = fromIntegral <$> belowHundred 1
+
+day :: Parser Int
+day = fromIntegral <$> belowHundred 3
+
+hour :: Parser Int
+hour = fromIntegral <$> belowHundred 2
+
+minute :: Parser Int
+minute = fromIntegral <$> belowHundred 5
+
+second :: Parser Pico
+second = fromIntegral <$> belowHundred 5
+
+-- | Match two-digit numbers up to n * 10 + 9.
+belowHundred :: Int -> Parser Integer
+belowHundred n = read <$> ((:) <$> oneOf ['0' .. intToDigit n] <*> count 1 digit)
+
+plusMinus :: Parser Rational
+plusMinus =
+    (negate <$> (char '-' *> decimal)) <|>
+    (char '+' *> decimal) <|>
+    decimal
+
+decimal :: Parser Rational
+decimal = try ((+) <$> (fromIntegral <$> integer) <*> fraction) <|> (fromIntegral <$> integer)
+
+fraction :: Parser Rational
+fraction = (\x -> fromIntegral (read x :: Integer) / (10 ^ length x)) <$> (char '.' *> many1 digit)
+
+integer :: Parser Integer
+integer = read <$> many1 digit
+
+
+--token :: Parser Token
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,26 +1,58 @@
 module Main where
 
-import Tt
+import Data.Time
+import Data.Ratio
 import Test.Hspec
-import qualified Data.Time as Time
+import Text.Parsec
+import Tt.Token
 
 main :: IO ()
 main = hspec
-  $ describe "Tt.parseToken" $ do
--- FIXME: Currently invalid because Token with ZonedTime cannot derive Eq,
--- Maybe fix it by turning it into (localTime, tzOffsetMinutes) or something?
+  $ describe "Token parser" $ do
     return ()
---    it "parses identifiers" $
---      parseToken "foo" `shouldBe` Sym "foo"
---    it "parses random test" $
---      parseToken "123stuff!" `shouldBe` Text "123stuff!"
---    it "parses date" $
---      parseToken "2012-12-21" `shouldBe` (Date $ Time.fromGregorian 2012 12 21)
---    it "parses time of day" $
---      parseToken "18:32:43+0200" `shouldBe` (Time $ Time.ZonedTime (Time.LocalTime (Time.ModifiedJulianDay 0) (Time.TimeOfDay 18 32 43)) (Time.TimeZone 120 False "0200"))
---    it "parses project identifiers" $
---      parseToken "+stuff" `shouldBe` Project "stuff"
---    it "parses attributes with string values" $
---      parseToken "prio:C" `shouldBe` Colon (Sym "prio") (Sym "C")
---    it "parses attributes with date values" $
---      parseToken "due:2018-03-20" `shouldBe` Colon (Sym "due") (Date $ Time.fromGregorian 2018 3 20)
+    it "parses identifiers" $
+      parseToken "foo" `shouldBe` Sym "foo"
+    it "parses date" $
+      parseToken "2012-12-21" `shouldBe` (Date $ fromGregorian 2012 12 21)
+    it "parses time of day" $
+      parseToken "18:32:43+0200" `shouldBe` Time (TimeOfDay 18 32 43, minutesToTimeZone 120)
+    it "parses time of day with negative tz" $
+      parseToken "18:32:43-0330" `shouldBe` Time (TimeOfDay 18 32 43, minutesToTimeZone (-210))
+    it "parses project identifiers" $
+      parseToken "+stuff" `shouldBe` Sym "+stuff"
+    it "parses attributes with string values" $
+      parseToken "prio:C" `shouldBe` Colon (Sym "prio") (Sym "C")
+    it "parses priority tags" $
+      parseToken "(A)" `shouldBe` Priority 'A'
+    it "parses attributes with date values" $
+      parseToken "due:2018-03-20" `shouldBe` Colon (Sym "due") (Date $ fromGregorian 2018 3 20)
+    it "parses integers" $
+      parseToken "123" `shouldBe` Number 123
+    it "parses negative integers" $
+      parseToken "-321" `shouldBe` Number (-321)
+    it "parses decimal parts" $
+      parseToken "1.01" `shouldBe` Number (101 % 100)
+    it "parses negatives and decimal parts" $
+      parseToken "-8.76" `shouldBe` Number (-876 % 100)
+    it "allows a plus sign in from of numbers" $
+      parseToken "+123" `shouldBe` Number 123
+    it "parses random text" $
+      parseToken "123stuff!" `shouldBe` Text "123stuff!"
+    it "parses random text from longer string" $
+      parseToken "123stuff! And things!" `shouldBe` Text "123stuff!"
+    it "does not parse punctuation junk" $
+      parseToken "(()" `shouldBe` Text "(()"
+    it "does not parse degenerate colon definitions" $
+      parseToken ":" `shouldBe` Text ":"
+    it "does not parse too long colon chains" $
+      parseToken "a:b:c" `shouldBe` Text "a:b:c"
+    it "does not parse invalid colon keys" $
+      parseToken "10:foo" `shouldBe` Text "10:foo"
+    it "does not parse invalid colon values" $
+      parseToken "a:12b" `shouldBe` Text "a:12b"
+
+parseToken :: String -> Token
+parseToken t = unwrap $ parse tokenParser "" t
+  where
+    unwrap (Right x) = x
+    unwrap (Left _) = error "Parsing failed"
