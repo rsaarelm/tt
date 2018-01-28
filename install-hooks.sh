631cfec9dbb7d7b91b8b1856a7e61b767229acf4
Risto Saarelma <risto.saarelma@iki.fi>
2018-01-28 14:52:21 +0200

    Hlint hook

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
 
-cat > .git/hooks/pre-push.sh << EOF
+cat > .git/hooks/pre-push << "EOF"
 stack test
 EOF
 
-chmod a+x .git/hooks/pre-push.sh
+chmod a+x .git/hooks/pre-push
+
+cat > .git/hooks/pre-commit << "EOF"
+# Taken from https://github.com/HIPERFIT/L0Language/blob/master/git-hooks/pre-commit
+
+fail() {
+    echo "Aborting commit due to verification errors."
+    echo "If you disagree, use git commit --no-verify."
+    exit 1
+}
+
+hlintable() {
+    ! egrep -q '{-# LANGUAGE.*QuasiQuotes' $1
+}
+
+l0_hlint() {
+    # Some hlint-suggestions are terrible, so ignore them here.
+    hlint -i "Reduce duplication" -i "Use import/export shortcut" "$@"
+}
+
+# Run hlint on changed files.
+for file in $(git diff-index --cached --name-only HEAD | egrep '\.l?hsc?$'); do
+    if [ -f $file ]; then
+        if hlintable $file; then
+            echo "Checking $file with hlint:"
+            if ! l0_hlint "$file"; then
+                fail
+            fi
+        fi
+    fi
+done
+EOF
+
+chmod a+x .git/hooks/pre-commit
