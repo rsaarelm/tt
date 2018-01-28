653c610ae59c4e7210f5459f9a19e8eff8e3e842
Risto Saarelma <risto.saarelma@iki.fi>
2018-01-28 17:40:41 +0200

    Main uses the new modular library, remove old Tt

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,15 @@ module Main where
 
 import Control.Monad
 import Data.Maybe
-import System.Directory (doesFileExist, getHomeDirectory)
-import System.FilePath (joinPath)
 import Options.Applicative
 import Data.Semigroup ((<>))
 import Data.Time
 import Text.Printf
-import Tt
+import Tt.Clock
+import Tt.Db
+import Tt.Session
+import Tt.Todo
+import Tt.Token
 
 main :: IO ()
 main = join $ execParser $ info (opts <**> helper) $
@@ -18,9 +20,9 @@ main = join $ execParser $ info (opts <**> helper) $
 
 opts :: Parser (IO ())
 opts = subparser $
-     command "in" (info (in_ <$> argument str (metavar "project") <*> many (argument str (metavar "description"))) $
+     command "in" (info (clockIn <$> argument str (metavar "project") <*> many (argument str (metavar "description"))) $
       progDesc "Clock in to a project")
-  <> command "out" (info (out <$> many (argument str (metavar "description"))) $
+  <> command "out" (info (clockOut <$> many (argument str (metavar "description"))) $
       progDesc "Clock out of the clocked in project")
   <> command "todo" (info (todo <$> some (argument str (metavar "description"))) $
       progDesc "Add a todo item from the command line")
@@ -33,62 +35,72 @@ opts = subparser $
   <> command "balance" (info (balance <$> argument str (metavar "project")) $
       progDesc "Show monthly flexitime balance on project")
 
-in_ :: String -> [String] -> IO ()
-in_ project text = do
-    current <- getCurrentProject
-    when (isJust current) (out [""])
-    line <- clockInPrefix
-    todoPath <- todoFilePath
-    appendFile todoPath $ showTokens (line ++ tokenize project ++ tokenize (unwords text)) ++ "\n"
-    putStrLn $ "Clocked into " ++ project ++ "."
 
-out :: [String] -> IO ()
-out text = do
-    line <- clockOutPrefix
-    todoPath <- todoFilePath
+clockIn :: String -> [String] -> IO ()
+clockIn project text = do
     current <- getCurrentProject
-    case current of Just project -> do
-                        appendFile todoPath $ showTokens (line ++ tokenize (unwords text)) ++ "\n"
-                        putStrLn $ "Clocked out of " ++ project ++ "."
-                    Nothing -> putStrLn "Error: Not clocked in a project"
+    when (isJust current) (clockOut [])
+    now <- getZonedTime
+    append $ clockInEntry now project (unwords text)
+    printf "Clocked into %s.\n" project
+
+clockOut :: [String] -> IO ()
+clockOut text = do
+    current <- getCurrentProject
+    case current of
+        Just project -> do
+            now <- getZonedTime
+            append $ clockOutEntry now (unwords text)
+            printf "Clocked out of %s.\n" project
+        Nothing -> printf "Error: Not clocked in a project.\n"
+
+getCurrentProject :: IO (Maybe String)
+getCurrentProject = do
+    clockDb <- fmap clocks db
+    return $ currentProject clockDb
+
 
 todo :: [String] -> IO ()
 todo text = do
-    line <- todoPrefix
-    todoPath <- todoFilePath
-    appendFile todoPath $ showTokens (line ++ tokenize (unwords text)) ++ "\n"
-    putStrLn "Task added"
+    now <- getZonedTime
+    let msg = unwords text
+    append $ todoEntry now msg
+    printf "Task added: %s\n" msg
 
 done :: [String] -> IO ()
 done text = do
-    line <- todoPrefix
-    todoPath <- todoFilePath
-    appendFile todoPath $ showTokens (tokenize "x" ++ line ++ tokenize (unwords text)) ++ "\n"
-    putStrLn "Done task added"
+    now <- getZonedTime
+    let msg = unwords text
+    append $ doneEntry now msg
+    printf "Done task added: %s\n" msg
+
 
 timeclock :: IO ()
 timeclock = do
-    db <- readDatabase
-    mapM_ (putStrLn . asTimeclock) $ toClockData db
+    clockDb <- clocks <$> db
+    mapM_ (putStrLn . asTimeclock) clockDb
+
 
 current :: IO ()
 current = do
-    clocks <- fmap toClockData readDatabase
-    zt <- getZonedTime
-    let current = currentProject clocks
-    let currentWork = filter (\x -> Just (sessionProject x) == current) (sessions zt clocks)
-    case current of Just project -> do
-                        let todaysWork = mapMaybe (clamp (daySpan zt)) currentWork
-                        let todaysTime = sum $ map sessionLength todaysWork
-                        printf "%s %s\n" project (showHours todaysTime)
-                    Nothing -> return ()
+    clockDb <- clocks <$> db
+    now <- getZonedTime
+    let current = currentProject clockDb
+    let currentWork = filter (\x -> Just (sessionProject x) == current) (sessions now clockDb)
+    case current of
+        Just project -> do
+            let todaysWork = mapMaybe (clamp (daySpan now)) currentWork
+            let todaysTime = sum $ map sessionLength todaysWork
+            printf "%s %s\n" project (showHours todaysTime)
+        Nothing -> return ()
+
 
 balance :: String -> IO ()
 balance project = do
-    clocks <- fmap toClockData readDatabase
-    zt <- getZonedTime
-    let work = mapMaybe (clamp (thisMonthUntilYesterday zt)) (filter (\x -> sessionProject x == project) (sessions zt clocks))
-    let numDays = daysCovered (zonedTimeZone zt) work
+    clockDb <- clocks <$> db
+    now <- getZonedTime
+    let work = mapMaybe (clamp (untilYesterday now)) (projectSessions now clockDb)
+    let numDays = daysCovered (zonedTimeZone now) work
     let amountWorked = sum $ map sessionLength work
     let nominalHour = nominalDay / 24
     -- TODO: Make the expected daily hours configurable.
@@ -96,43 +108,24 @@ balance project = do
     let balance = amountWorked - targetAmount
     printf "%s hours over %d days this month in project %s\n" (showHours amountWorked) numDays project
     printf "Flexitime balance %s\n" (showHours balance)
+  where
+    projectSessions now clockDb =
+        filter (\x -> sessionProject x == project) (sessions now clockDb)
 
-
-thisMonthUntilYesterday :: ZonedTime -> (UTCTime, UTCTime)
-thisMonthUntilYesterday zt =
+untilYesterday :: ZonedTime -> (UTCTime, UTCTime)
+untilYesterday t =
     (begin, end)
     where
-        (begin, _) = monthSpan zt
-        (end, _) = daySpan zt
+        (begin, _) = monthSpan t
+        (end, _) = daySpan t
 
-parseFile :: FilePath -> IO [[Token]]
-parseFile path = do
-    exists <- doesFileExist path
-    if exists then do
-        contents <- readFile path
-        return $ map tokenize $ lines contents
-    else return []
 
-readDatabase :: IO [[Token]]
-readDatabase = do
-    donePath <- doneFilePath
-    done <- parseFile donePath
-    todoPath <- todoFilePath
-    todo <- parseFile todoPath
-    return (done ++ todo)
+db :: IO Db
+db = do
+    conf <- dbConf
+    readDb conf
 
-getCurrentProject :: IO (Maybe String)
-getCurrentProject = do
-    clocks <- fmap toClockData readDatabase
-    return $ currentProject clocks
-
-todoFilePath :: IO FilePath
-todoFilePath = homeFilePath "todo.txt"
-
-doneFilePath :: IO FilePath
-doneFilePath = homeFilePath "done.txt"
-
-homeFilePath :: FilePath -> IO FilePath
-homeFilePath filename = do
-    home <- getHomeDirectory
-    return $ joinPath [home, filename]
+append :: Entry -> IO ()
+append entry = do
+    conf <- dbConf
+    dbAppend conf entry
