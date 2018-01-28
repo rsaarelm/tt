2ab81656694df436485a14cb0d5189bff37431b5
Risto Saarelma <risto.saarelma@iki.fi>
2018-01-28 14:38:32 +0200

    Db module

---
+++ src/Tt/Db.hs
@@ -0,0 +1,52 @@
+module Tt.Db (
+  Entry,
+  Db,
+  DbConf,
+  dbConf,
+  readDb,
+  showTokens,
+  tokenize,
+  dbAppend,
+) where
+
+import Control.Monad
+import System.FilePath (joinPath)
+import System.Directory (doesFileExist, getHomeDirectory)
+import Tt.Token
+import Text.Parsec
+
+type Entry = [Token]
+type Db = [Entry]
+
+-- | List of paths used for the database, active todo list should be last.
+type DbConf = [FilePath]
+
+readDb :: DbConf -> IO Db
+readDb conf = join <$> traverse parseFile conf
+
+dbConf :: IO DbConf
+dbConf = traverse homeFilePath ["done.txt", "todo.txt"]
+  where
+    homeFilePath filename = do
+        home <- getHomeDirectory
+        return $ joinPath [home, filename]
+
+parseFile :: FilePath -> IO Db
+parseFile path = do
+    exists <- doesFileExist path
+    if exists then do
+        contents <- readFile path
+        return $ map tokenize $ lines contents
+    else error ("File " ++ path ++ " not found")
+
+showTokens :: Entry -> String
+showTokens = unwords . map showToken
+
+tokenize :: String -> Entry
+tokenize text = (unwrap . parse tokenParser "") <$> words text
+  where
+    unwrap (Right x) = x
+    unwrap (Left _) = error ("Parsing '" ++ text ++ "' failed.")
+
+dbAppend :: DbConf -> Entry -> IO ()
+dbAppend conf entry = appendFile (last conf) (showTokens entry ++ "\n")
