af26c25a259662cc1dacc50f72a2f1a191d0eb82
Risto Saarelma <risto.saarelma@iki.fi>
2018-02-04 10:00:05 +0200

    Remove interim command
    Seems marginal, and you can just do
    
        tt out "message"; tt in project
    
    from the command line to the same effect.

--- src/Main.hs
+++ src/Main.hs
@@ -39,12 +39,6 @@ opts =
          $ progDesc "Clock out of the clocked in project"
          )
     <> command
-         "interim"
-         ( info (clockOutIn <$> many (argument str (metavar "description")))
-         $ progDesc
-             "Clock out with an interim done message, then clock back in for more work"
-         )
-    <> command
          "todo"
          ( info (todo <$> some (argument str (metavar "description")))
          $ progDesc "Add a todo item from the command line"
@@ -92,16 +86,6 @@ clockOut text = do
       now <- getZonedTime
       append $ clockOutEntry now (unwords text)
       printf "Clocked out of %s.\n" project
-    Nothing -> printf "Error: Not clocked in a project.\n"
-
-clockOutIn :: [String] -> IO ()
-clockOutIn text = do
-  current <- getCurrentProject
-  case current of
-    Just project -> do
-      clockOut text
-      clockIn project                             []
-      printf  "Entered interim message for %s.\n" project
     Nothing -> printf "Error: Not clocked in a project.\n"
 
 getCurrentProject :: IO (Maybe String)
