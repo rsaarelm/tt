5c103a55680ed5d3cf58492d170cd3d3ea38e406
Risto Saarelma <risto.saarelma@iki.fi>
2018-08-29 15:00:24 +0300

    Add development tools to NixOS shell

--- shell.nix
+++ shell.nix
@@ -4,6 +4,11 @@ let
 
   inherit (nixpkgs) pkgs;
 
+  # Haskell IDE Engine. Idea was to use this with nvin language server, but it
+  # takes forever to build and I'm not sure it actually worked with Vim...
+  #
+  # hie = import (fetchTarball "https://github.com/domenkozar/hie-nix/archive/e3113da93b479bec3046e67c0123860732335dd9.tar.gz") { inherit pkgs; };
+
   f = { mkDerivation, base, containers, directory, filepath, hspec
       , intervals, optparse-applicative, parsec, stdenv, time
       }:
@@ -24,6 +29,14 @@ let
         homepage = "https://github.com/rsaarelm/tt";
         description = "Command line time tracking and todo tool";
         license = stdenv.lib.licenses.bsd3;
+
+        buildDepends = [
+          pkgs.cabal-install
+          pkgs.hlint
+          haskellPackages.brittany
+
+          # hie.hies
+        ];
       };
 
   haskellPackages = if compiler == "default"
