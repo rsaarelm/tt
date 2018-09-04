1d2526e24c2c03ddf317935a915803c8a6987ab8
Risto Saarelma <risto.saarelma@iki.fi>
2018-09-04 12:21:46 +0300

    Default.nix calling higher-level NixOS machinery

--- default.nix
+++ default.nix
@@ -1,25 +1,26 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-
-let
-  f = { mkDerivation, base, hspec, intervals, optparse-applicative, parsec, stdenv }:
-  mkDerivation {
-    pname = "tt";
-    version = "0.1.0.0";
-    src = ./.;
-    isLibrary = false;
-    isExecutable = true;
-    executableHaskellDepends = [
-      base hspec intervals optparse-applicative parsec ];
-
-    homepage = "https://github.com/rsaarelm/tt/";
-    description = "Command line time tracking and todo tool";
-    license = stdenv.lib.licenses.bsd3;
-  };
-
-  haskellPackages = if compiler == "default"
-    then nixpkgs.haskellPackages
-    else nixpkgs.haskell.packages.${compiler};
-
-  drv = haskellPackages.callPackage f {};
-in
-  if nixpkgs.lib.inNixShell then drv.env else drv
+{ pkgs ? import <nixpkgs> {} }: pkgs.haskellPackages.developPackage
+  { root = ./.;
+    overrides = self: super:
+      { # Don't run a package's test suite
+        # foo = pkgs.haskell.lib.dontCheck pkgs.haskellPackages.foo;
+        #
+        # Don't enforce package's version constraints
+        # bar = pkgs.haskell.lib.doJailbreak pkgs.haskellPackages.bar;
+        #
+        # To discover more functions that can be used to modify haskell
+        # packages, run "nix-repl", type "pkgs.haskell.lib.", then hit
+        # <TAB> to get a tab-completed list of functions.
+      };
+    source-overrides =
+      { # Use a specific hackage version
+        # optparse-applicative = "0.14.0.0";
+        #
+        # Use a particular commit from github
+        # my-private-package = pkgs.fetchFromGitHub
+        #   { owner = "my-github-username";
+        #     repo = "my-private-package";
+        #     rev = "561de381bcccfe6792f2908a5022449a05ae0050";
+        #     sha256 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        #   };
+      };
+  }
